6f01a190db527aab02a12ba955c57081142815d1
Francis Kyle <franciskyle6969@gmail.com>
2026-03-26 00:40:47 +0800

    feat: add favicon and improve page title
    - Added favicon link to public/favicon.ico in app.blade.php
    - Added apple-touch-icon for iOS devices
    - Improved page title to include 'Barangay Blotter System'
    - Favicon will now display in browser tabs

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,12 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{ config('app.name') }}</title>
+    <title>{{ config('app.name') }} - Barangay Blotter System</title>
+    
+    <!-- Favicon -->
+    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
+    <link rel="apple-touch-icon" href="{{ asset('favicon.ico') }}">
+    
     @vite(['resources/css/app.css', 'resources/js/app.jsx'])
     @inertiaHead
 </head>
